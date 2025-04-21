2378cfab23ec109eeafd88a134486cbac58355ba
Pindosaputra123 <pindoajja864@gmail.com>
2025-04-21 08:14:21 +0700

    Tampilan Form Laporan

---
+++ lapor.php
@@ -0,0 +1,278 @@
+<?php
+    require_once("private/database.php");
+    $statement = $db->query("SELECT id FROM `laporan` ORDER BY id DESC LIMIT 1");
+    // $cekk = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($statement->rowCount()>0) {
+        foreach ($statement as $key ) {
+            // get max id from tabel laporan
+            $max_id = $key['id']+1;
+        }
+    }
+    if ($statement->rowCount()<1) {
+        $max_id = 100;
+    }
+?>
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Saur Warga</title>
+    <link rel="shortcut icon" href="images/samblog.svg">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- font Awesome CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Main Styles CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/lapor.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="shadow">
+    <?php
+// Fungsi untuk menentukan halaman aktif
+function isActive($page) {
+    return basename($_SERVER['PHP_SELF']) == $page ? 'active-menu' : '';
+}
+?>
+    <div class="navbar">
+        <div class="main-logo">
+            <a href="/">
+                <img src="images/samblog.svg" alt="Logo Sambat" class="main-logo">
+            </a>
+            <div class="sub-tem">
+                <h1>Saur Warga</h1>
+            </div>
+        </div>
+
+        <div class="menu">
+            <a href="index.php" class="<?= isActive('index.php') ?>" target="_top">HOME</a>
+            <a href="lapor.php" class="<?= isActive('lapor.php') ?>" target="_top">LAPOR</a>
+            <a href="lihat.php" class="<?= isActive('lihat.php') ?>" target="_top">LIHAT LAPORAN</a>
+            <a href="cara.php" class="<?= isActive('cara.php') ?>" target="_top">PROFIL DINAS</a>
+            <a href="faq.php" class="<?= isActive('faq.php') ?>" target="_top">TENTANG</a>
+        </div>
+
+        <?php
+        session_start(); // Start session to store user information
+        $username = isset($_SESSION['username']) ? $_SESSION['username'] : null;
+        ?>
+
+        <?php if ($username) : ?> 
+            <div class="logsig">
+                <a href="/Pengaduan-Masyarakat/profile.php">
+                    <button class="login-new-btn">
+                        <?= htmlspecialchars($username) ?>
+                    </button>
+                </a>
+            </div>
+        <?php else: ?>
+            <div class="logsig">
+                <a href="/SAMBATRAKYAT/login.php">
+                    <button class="login-btn">Masuk</button>
+                </a>
+                <a href="/SAMBATRAKYAT/signin.php">
+                    <button class="signup-btn">Daftar</button>
+                </a>
+            </div>
+        <?php endif; ?>
+    </div>
+
+
+        <!-- content -->
+        <div class="main-content">
+
+            <h3 style="font-weight: bold">Buat Laporan</h3>
+            <hr/>
+            <div class="row">
+                <div class="col-md-8 card-shadow-2 form-custom">
+                    <form class="form-horizontal" role="form" method="post" action="private/validasi">
+                        <div class="form-group">
+                            <label for="nomor" class="col-sm-3 control-label">Nomor Pengaduan</label>
+                            <div class="col-sm-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="glyphicon glyphicon-exclamation-sign"></span></div>
+                                    <input type="text" class="form-control" id="nomor" name="nomor" value="<?php echo $max_id; ?>" readonly>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="nama" class="col-sm-3 control-label">Nama</label>
+                            <div class="col-sm-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span></div>
+                                    <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Lengkap" value="<?= @$_GET['nama'] ?>" required>
+                                </div>
+                                <p class="error"><?= @$_GET['namaError'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="col-sm-3 control-label">Email</label>
+                            <div class="col-sm-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></div>
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?= @$_GET['email'] ?>" required>
+                                </div>
+                                <p class="error"><?= @$_GET['emailError'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="telpon" class="col-sm-3 control-label">Telpon</label>
+                            <div class="col-sm-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="glyphicon glyphicon-phone"></span></div>
+                                    <input type="text" class="form-control" id="telpon" name="telpon" placeholder="087123456789" value="<?= @$_GET['telpon'] ?>" required>
+                                </div>
+                                <p class="error"><?= @$_GET['telponError'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="alamat" class="col-sm-3 control-label">Alamat</label>
+                            <div class="col-sm-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="glyphicon glyphicon-home"></span></div>
+                                    <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Alamat" value="<?= @$_GET['alamat'] ?>" required>
+                                </div>
+                                <p class="error"><?= @$_GET['alamatError'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="tujuan" class="col-sm-3 control-label">Tujuan Pengaduan</label>
+                            <div class="col-sm-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="glyphicon glyphicon-random"></span></div>
+                                    <select class="form-control" name="tujuan">
+                                        <option value="1">Pelayanan Pendaftaran Penduduk</option>
+                                        <option value="2">Pelayanan Pencatatan Sipil</option>
+                                        <option value="3">Pengelolaan Informasi Administrasi Kependudukan</option>
+                                        <option value="4">Pemanfaatan Data Dan Inovasi Pelayanan</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="pengaduan" class="col-sm-3 control-label">Isi Pengaduan</label>
+                            <div class="col-sm-9">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></div>
+                                    <textarea class="form-control" rows="4" name="pengaduan" placeholder="Tuliskan Isi Pengaduan" required><?= @$_GET['pengaduan'] ?></textarea>
+                                </div>
+                                <p class="error"><?= @$_GET['pengaduanError'] ?></p>
+                            </div>
+                        </div>
+            
+                        </div>
+                       
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-3">
+                                <input id="submit" name="submit" type="submit" value="Kirim Pengaduan" class="btn btn-primary-custom form-shadow">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-2">
+                                <p class="error"><em>* Catat Nomor Pengaduan Untuk Melihat Status Pengaduan</em></p>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+
+            <!-- link to top -->
+            <a id="top" href="#" onclick="topFunction()">
+                <i class="fa fa-arrow-circle-up"></i>
+            </a>
+            <script>
+            // When the user scrolls down 100px from the top of the document, show the button
+            window.onscroll = function() {scrollFunction()};
+            function scrollFunction() {
+                if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                    document.getElementById("top").style.display = "block";
+                } else {
+                    document.getElementById("top").style.display = "none";
+                }
+            }
+            // When the user clicks on the button, scroll to the top of the document
+            function topFunction() {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            }
+            </script>
+            <!-- link to top -->
+
+
+            <!-- /.section -->
+            <hr>
+        </div>
+
+        <!-- Footer -->
+       <!-- Footer -->
+       <footer class="footer text-center">
+            <div class="row">
+                <div class="col-md-4 mb-5 mb-lg-0">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <i class="fa fa-top fa-map-marker"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <h4 class="text-uppercase mb-4">Kantor</h4>
+                        </li>
+                    </ul>
+                    <p class="mb-0">
+                        Jl. A. Yani No. 1 Karawang
+                        <br>Karawang, Jawa Barat
+                    </p>
+                </div>
+                <div class="col-md-4 mb-5 mb-lg-0">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <i class="fa fa-top fa-rss"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <h4 class="text-uppercase mb-4">Sosial Media</h4>
+                        </li>
+                    </ul>
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://twitter.com/KarawangDay?lang=en">
+                                <i class="fa fa-fw fa-twitter"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <i class="fa fa-top fa-envelope-o"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <h4 class="text-uppercase mb-4">Kontak</h4>
+                        </li>
+                    </ul>
+                    <p class="mb-0">
+                        +62 812-3456-7890 <br>
+                        https://karawangkab.go.id/opd <br>
+                        karawangpemkab@gmail.com
+                    </p>
+                </div>
+            </div>
+        </footer>
+        <!-- /footer -->
+
+    <div class="copyright">
+        <p style="text-align: center; color: white">v-1.0 | Copyright &copy; Pemerintahan Kabupaten Karawang</p>
+    </div>
+        <!-- shadow -->
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
