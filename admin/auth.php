498424f77853d8a2e30d71c3d842a50598ad5ce4
Pindosaputra123 <pindoajja864@gmail.com>
2025-05-07 15:41:29 +0700

    Lindungi Halaman Admin Dan Menyimpan URL Tujuan Sebelum Login

---
+++ admin/auth.php
@@ -0,0 +1,12 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Simpan URL halaman yang diminta sebelum login
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+    $_SESSION['redirect_url'] = $_SERVER['REQUEST_URI']; // Simpan halaman sebelumnya
+    header("Location: /Pengaduan-Masyarakat/login.php");
+    exit();
+}
+?>
